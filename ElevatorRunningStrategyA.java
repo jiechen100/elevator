25c868a265d93c85f0feb006af8caee0b8d34fd7
jiechen100 <jiechen100@yahoo.com>
2017-02-03 16:32:29 -0600

    Create ElevatorRunningStrategyA.java

---
+++ ElevatorRunningStrategyA.java
@@ -0,0 +1,17 @@
+package com.jie.elevator;
+
+import java.util.List;
+
+public class ElevatorRunningStrategyA implements ElevatorRunningModeStrategy {
+
+	public Command nextCommand(List<Command> cmds) {
+		if (cmds == null || cmds.size() == 0) {
+			return new Command(ControlCommandEnum.STOP);
+		}
+
+		Command cmd = cmds.get(0);
+		cmds.remove(0);
+
+		return cmd;
+	}
+}
